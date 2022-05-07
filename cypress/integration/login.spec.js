ac0e0129daba716738f6ff18a18a42ce5aeb29b5
Patricia Cristina <patcrisaragao@gmail.com>
2022-05-07 12:11:14 -0400

    Adicionando Hooks e Faker

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,11 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -12,7 +15,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -22,7 +24,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('@teste.com')
         cy.get('.woocommerce-form > .button').click()
